342b6d65afa28aca4c2adc705cd7d914993c0cce
kazumi <kazumi121589@yahoo.co.jp>
2022-08-21 11:45:20 +0900

    ルーティングを絞り込んだ

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
   namespace 'api' do
     namespace 'v1' do
-      resources :users
-      resources :companies
-      resources :works
+      resources :users, only: [:index, :create, :update, :destroy]
+      resources :companies, only: [:index, :create, :update, :destroy]
+      resources :works, only: [:index, :create, :update, :destroy]
     end
   end
 end
