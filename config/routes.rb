bb8419a7efafb56cdaee83843891125a58510771
kazumi <kazumi121589@yahoo.co.jp>
2022-09-05 23:35:11 +0900

    退会時にサーバーサイドのデータが削除されるようにした

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
       get 'user', to: 'users#show'
       patch 'user', to: 'users#update'
       delete 'user', to: 'users#destroy'
-      delete 'authentication', to: 'authentications#destroy'
+      resources :authentications, only: [:destroy]
       resources :companies, :works, only: [:index, :create, :update, :destroy]
       resources :exchange_rates, only: [:index]
     end
