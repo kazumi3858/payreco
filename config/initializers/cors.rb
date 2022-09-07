0fd8e3cace2d749d186955375d99f659d859b94e
kazumi <kazumi121589@yahoo.co.jp>
2022-09-07 21:05:15 +0900

    検証のためcorsのoriginを変更した

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'http://localhost:3001', 'https://payreco-kazumi3858.vercel.app', 'https://payreco.vercel.app', 'https://payreco-git-main-kazumi3858.vercel.app'
+    origins "*" #'http://localhost:3001', 'https://payreco-kazumi3858.vercel.app', 'https://payreco.vercel.app', 'https://payreco-git-main-kazumi3858.vercel.app'
 
     resource "*",
       headers: :any,
