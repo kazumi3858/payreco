cd0302254c01442b5df890b29de4ce5b3a7762d9
kazumi <kazumi121589@yahoo.co.jp>
2022-09-08 10:58:52 +0900

    ルーティングを修正した

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  match '*path' => 'options_request#preflight', via: :options
   namespace 'api' do
     namespace 'v1' do
+      match '*path' => 'options_request#preflight', via: :options
       get 'user', to: 'users#show'
       patch 'user', to: 'users#update'
       delete 'user', to: 'users#destroy'
