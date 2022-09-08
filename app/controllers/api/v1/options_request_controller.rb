8a523d1edd1a8529f86f48f95ebc2bb61900cc4b
kazumi <kazumi121589@yahoo.co.jp>
2022-09-08 12:50:33 +0900

    options request controllerを検証のためコメントアウトした

--- app/controllers/api/v1/options_request_controller.rb
+++ app/controllers/api/v1/options_request_controller.rb
@@ -1,26 +1,26 @@
-# frozen_string_literal: true
+# # frozen_string_literal: true
 
-module Api
-  module V1
-    class OptionsRequestController < ApplicationController
-      before_action :preflight
+# module Api
+#   module V1
+#     class OptionsRequestController < ApplicationController
+#       before_action :preflight
 
-      def cors_preflight_check
-        return unless request.method == 'OPTIONS'
+#       def cors_preflight_check
+#         return unless request.method == 'OPTIONS'
 
-        cors_set_access_control_headers
-        render json: {}, status: :no_content
-      end
+#         cors_set_access_control_headers
+#         render json: {}, status: :no_content
+#       end
 
-      protected
+#       protected
 
-      def preflight
-        response.headers['Access-Control-Allow-Origin'] = '*'
-        response.headers['Access-Control-Allow-Methods'] = 'POST, GET, PUT, PATCH, DELETE, OPTIONS'
-        response.headers['Access-Control-Allow-Headers'] =
-          'Origin, X-Requested-With, Content-Type, Authorization, Accept'
-        response.headers['Access-Control-Max-Age'] = '1728000'
-      end
-    end
-  end
-end
+#       def preflight
+#         response.headers['Access-Control-Allow-Origin'] = '*'
+#         response.headers['Access-Control-Allow-Methods'] = 'POST, GET, PUT, PATCH, DELETE, OPTIONS'
+#         response.headers['Access-Control-Allow-Headers'] =
+#           'Origin, X-Requested-With, Content-Type, Authorization, Accept'
+#         response.headers['Access-Control-Max-Age'] = '1728000'
+#       end
+#     end
+#   end
+# end
