c1fdde1a02d067b768f04147d5f0f222eed0540a
kazumi <kazumi121589@yahoo.co.jp>
2022-09-08 22:59:47 +0900

    application controllerでoptionsリクエストを捌くようにした

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,7 @@
 
 class ApplicationController < ActionController::API
   include ActionController::HttpAuthentication::Token::ControllerMethods
+  before_action :cors_set_access_control_headers
   before_action :authenticate
 
   class AuthenticationError < StandardError; end
@@ -19,9 +20,24 @@ class ApplicationController < ActionController::API
     @current_user.save!
   end
 
-  private
-
   def not_authenticated
     render json: { error: { messages: ['ログインしてください'] } }, status: :unauthorized
+  end
+
+  def cors_preflight_check
+    return unless request.method == 'OPTIONS'
+
+    cors_set_access_control_headers
+    render plain: ''
+  end
+
+  protected
+
+  def cors_set_access_control_headers
+    response.headers['Access-Control-Allow-Origin'] = '*'
+    response.headers['Access-Control-Allow-Methods'] = 'POST, GET, PUT, PATCH, DELETE, OPTIONS'
+    response.headers['Access-Control-Allow-Headers'] =
+      'Origin, Content-Type, Accept, Authorization, Token, Auth-Token, Email, X-User-Token, X-User-Email'
+    response.headers['Access-Control-Max-Age'] = '1728000'
   end
 end
