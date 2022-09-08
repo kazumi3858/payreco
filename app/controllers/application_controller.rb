66a6011e8858c39fd18eb151a6844e2bf34de07f
kazumi <kazumi121589@yahoo.co.jp>
2022-09-08 11:16:04 +0900

    forgery protectionを有効にした

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::API
+  include ActionController::RequestForgeryProtection
   include ActionController::HttpAuthentication::Token::ControllerMethods
   before_action :authenticate
 
