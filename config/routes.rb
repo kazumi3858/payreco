b4f04d62bb31b8cfd3a9f6fd8cbbd25d52788649
kazumi <kazumi121589@yahoo.co.jp>
2022-09-08 12:58:37 +0900

    使わなくなったファイルを削除した

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
   namespace 'api' do
     namespace 'v1' do
-      # match '*path' => 'options_request#cors_preflight_check', via: :options
       get 'user', to: 'users#show'
       patch 'user', to: 'users#update'
       delete 'user', to: 'users#destroy'
