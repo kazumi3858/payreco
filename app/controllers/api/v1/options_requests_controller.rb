83fdd9fb50c173af4b6011b1b9e5b70442b89d65
kazumi <kazumi121589@yahoo.co.jp>
2022-09-08 11:34:21 +0900

    検証のためprotect forgeryを外した

--- app/controllers/api/v1/options_requests_controller.rb
+++ app/controllers/api/v1/options_requests_controller.rb
@@ -7,15 +7,13 @@ module Api
       ACCESS_CONTROL_ALLOW_HEADERS = %w[Accept Origin Content-Type Authorization].freeze
       ACCESS_CONTROL_MAX_AGE = 86_400
 
-      protect_from_forgery except: :preflight
-
       before_action :set_preflight_headers!, only: [:preflight]
 
       def preflight
         response.headers['Access-Control-Max-Age'] = ACCESS_CONTROL_MAX_AGE
         response.headers['Access-Control-Allow-Headers'] = ACCESS_CONTROL_ALLOW_HEADERS.join(',')
         response.headers['Access-Control-Allow-Methods'] = ACCESS_CONTROL_ALLOW_METHODS.join(',')
-        response.headers['Access-Control-Allow-Origin'] = '*'
+        response.headers['Access-Control-Allow-Origin'] = 'http://localhost:30001'
         head :ok
       end
     end
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,6 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::API
-  include ActionController::RequestForgeryProtection
   include ActionController::HttpAuthentication::Token::ControllerMethods
   before_action :authenticate
 
