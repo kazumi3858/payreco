d85da1e071835a831b9e8d64cd074bb5f8d362e3
kazumi <kazumi121589@yahoo.co.jp>
2022-09-09 23:56:12 +0900

    rateを外部APIから取得できるようにした

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,6 @@
 
 class ApplicationController < ActionController::API
   include ActionController::HttpAuthentication::Token::ControllerMethods
-  # before_action :cors_set_access_control_headers
   before_action :authenticate
 
   class AuthenticationError < StandardError; end
@@ -23,21 +22,4 @@ class ApplicationController < ActionController::API
   def not_authenticated
     render json: { error: { messages: ['ログインしてください'] } }, status: :unauthorized
   end
-
-  # def cors_preflight_check
-  #   return unless request.method == 'OPTIONS'
-
-  #   cors_set_access_control_headers
-  #   head :no_content
-  # end
-
-  # protected
-
-  # def cors_set_access_control_headers
-  #   response.headers['Access-Control-Allow-Origin'] = '*'
-  #   response.headers['Access-Control-Allow-Methods'] = 'POST, GET, PUT, PATCH, DELETE, OPTIONS, HEAD'
-  #   response.headers['Access-Control-Allow-Headers'] =
-  #     'Origin, Content-Type, Accept, Authorization, Token, Auth-Token, X-XSRF-TOKEN'
-  #   response.headers['Access-Control-Max-Age'] = '1728000'
-  # end
 end
