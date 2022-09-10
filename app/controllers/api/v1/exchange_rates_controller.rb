f750054a76ea7e66654ea5ef6be0c9bbc98ddb8c
kazumi <kazumi121589@yahoo.co.jp>
2022-09-10 23:40:44 +0900

    exchange ratesコントローラー内の一部メソッドをモデルに移動した

--- app/controllers/api/v1/exchange_rates_controller.rb
+++ app/controllers/api/v1/exchange_rates_controller.rb
@@ -6,9 +6,6 @@ require 'json'
 module Api
   module V1
     class ExchangeRatesController < ApplicationController
-      CURRENCY_NAMES = %w[円 米ドル ユーロ 英ポンド インドルピー 豪ドル
-                          カナダドル ランド NZドル SGドル 人民元 スイスフラン].freeze
-
       def index
         exchange_rates = ExchangeRate.order(:created_at)
         create_or_update_todays_rate if exchange_rates.empty? || exchange_rates.last.old_data?
@@ -16,24 +13,9 @@ module Api
       end
 
       def create_or_update_todays_rate
+        this_year_and_this_month = Time.zone.today.to_s.delete('-').slice(0..5).to_i
         exchange_rate = ExchangeRate.find_or_initialize_by(year_and_month: this_year_and_this_month)
-        exchange_rate.update(year_and_month: this_year_and_this_month, exchange_rate_list: rate_data)
-      end
-
-      private
-
-      def rate_data
-        url = "https://v6.exchangerate-api.com/v6/#{ENV.fetch('RATE_API_KEY', nil)}/latest/JPY"
-        uri = URI(url)
-        response = Net::HTTP.get(uri)
-        response_obj = JSON.parse(response)
-        rate = response_obj['conversion_rates'].slice('JPY', 'USD', 'EUR', 'GBP', 'INR', 'AUD', 'CAD', 'ZAR', 'NZD',
-                                                      'SGD', 'CNY', 'CHF')
-        rate.transform_keys!.with_index { |_key, index| CURRENCY_NAMES[index] }
-      end
-
-      def this_year_and_this_month
-        Time.zone.today.to_s.delete('-').slice(0..5).to_i
+        exchange_rate.update(year_and_month: this_year_and_this_month, exchange_rate_list: ExchangeRate.data)
       end
     end
   end
--- app/models/exchange_rate.rb
+++ app/models/exchange_rate.rb
@@ -1,7 +1,23 @@
 # frozen_string_literal: true
 
 class ExchangeRate < ApplicationRecord
+  CURRENCY_NAMES = %w[円 米ドル ユーロ 英ポンド インドルピー 豪ドル
+                      カナダドル ランド NZドル SGドル 人民元 スイスフラン].freeze
+
   def old_data?
     created_at < Time.zone.now.beginning_of_day
+  end
+
+  def self.data
+    response = JSON.parse(call_api)
+    rate = response['conversion_rates'].slice('JPY', 'USD', 'EUR', 'GBP', 'INR', 'AUD', 'CAD', 'ZAR', 'NZD',
+                                              'SGD', 'CNY', 'CHF')
+    rate.transform_keys!.with_index { |_key, index| CURRENCY_NAMES[index] }
+  end
+
+  def self.call_api
+    url = "https://v6.exchangerate-api.com/v6/#{ENV.fetch('RATE_API_KEY', nil)}/latest/JPY"
+    uri = URI(url)
+    Net::HTTP.get(uri)
   end
 end
