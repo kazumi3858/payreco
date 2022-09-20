cb7ec8f423c7ace66c1f6cb95003fdd632272379
kazumi <kazumi121589@yahoo.co.jp>
2022-09-20 23:22:18 +0900

    companies APIのテストを書いた

--- spec/requests/api/v1/companies_spec.rb
+++ spec/requests/api/v1/companies_spec.rb
@@ -9,7 +9,7 @@ RSpec.describe 'Api::V1::Companies', type: :request do
     authorization_stub
   end
 
-  describe 'GET /company' do
+  describe 'GET /api/v1/companies' do
     before do
       create_list(:company, 3, user_id: current_user.id)
       get '/api/v1/companies'
@@ -22,6 +22,48 @@ RSpec.describe 'Api::V1::Companies', type: :request do
 
     it 'has expected responses' do
       expect(JSON.parse(response.body).length).to eq(3)
+    end
+  end
+
+  describe 'POST /api/v1/companies' do
+    let(:headers) { { 'Content-Type' => 'application/json' } }
+
+    context 'when create' do
+      let(:params) { attributes_for(:company).to_json }
+
+      it 'returns expected status' do
+        post '/api/v1/companies', params: params, headers: headers
+        expect(response).to have_http_status(:created)
+        assert_request_schema_confirm
+      end
+    end
+  end
+
+  describe 'PATCH /api/v1/companies/:id' do
+    let(:headers) { { 'Content-Type' => 'application/json' } }
+
+    context 'when update' do
+      let(:params) { attributes_for(:company).to_json }
+
+      it 'returns expected status' do
+        company = create(:company, user_id: current_user.id)
+        patch "/api/v1/companies/#{company.id}", params: params, headers: headers
+        expect(response).to have_http_status(:ok)
+        assert_request_schema_confirm
+      end
+    end
+  end
+
+  describe 'DELETE /api/v1/companies/:id' do
+    let(:headers) { { 'Content-Type' => 'application/json' } }
+
+    context 'when delete' do
+      it 'returns expected status' do
+        company = create(:company, user_id: current_user.id)
+        delete "/api/v1/companies/#{company.id}", headers: headers
+        expect(response).to have_http_status(:no_content)
+        assert_request_schema_confirm
+      end
     end
   end
 end
