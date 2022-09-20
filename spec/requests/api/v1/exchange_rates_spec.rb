fd0ae508d960df89359293e3e69880875da217c3
kazumi <kazumi121589@yahoo.co.jp>
2022-09-21 01:12:54 +0900

    exchange rates APIのテストを書いた

--- spec/requests/api/v1/exchange_rates_spec.rb
+++ spec/requests/api/v1/exchange_rates_spec.rb
@@ -3,7 +3,12 @@
 require 'rails_helper'
 
 RSpec.describe 'Api::V1::ExchangeRates', type: :request do
-  describe 'GET /exchange_rates' do
+  before do
+    exchange_rate_api_stub
+    api_token_stub
+  end
+
+  describe 'GET /api/v1/exchange_rates' do
     before do
       create_list(:exchange_rate, 2)
       get '/api/v1/exchange_rates'
@@ -16,6 +21,13 @@ RSpec.describe 'Api::V1::ExchangeRates', type: :request do
 
     it 'has expected responses' do
       expect(JSON.parse(response.body).length).to eq(2)
+    end
+  end
+
+  describe 'POST /api/v1/exchange_rates' do
+    it 'success' do
+      post '/api/v1/exchange_rates'
+      expect(response).to have_http_status :ok
     end
   end
 end
--- spec/support/authorization_helper.rb
+++ spec/support/authorization_helper.rb
@@ -1,7 +1,20 @@
 # frozen_string_literal: true
 
 module AuthorizationHelper
+  EXCHANGE_RATE_JSON = { conversion_rates: { JPY: 2, AUD: 0.01041, EUR: 0.01249, CHF: 0.006737, CAD: 6.4284,
+                                             CNY: 0.04887, EGP: 0.1352, GBP: 0.006114, INR: 0.01978, HKD: 0.05481,
+                                             HRK: 0.05254, MWK: 7.2214, NZD: 0.01173, SDG: 3.9553, SEK: 0.07530,
+                                             SGD: 0.009833, SZL: 0.1235, USD: 0.006980, ZAR: 0.1235 } }.to_json
+
   def authorization_stub
     allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(current_user) # rubocop:disable RSpec/AnyInstance
+  end
+
+  def exchange_rate_api_stub
+    allow(ExchangeRate).to receive(:call_api).and_return(EXCHANGE_RATE_JSON)
+  end
+
+  def api_token_stub
+    allow_any_instance_of(Api::V1::ExchangeRatesController).to receive(:valid_token?).and_return(true) # rubocop:disable RSpec/AnyInstance
   end
 end
