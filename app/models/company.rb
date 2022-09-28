e5b344f732f4fd6385f3f1a856e1c0ae0f4b0f71
kazumi <kazumi121589@yahoo.co.jp>
2022-09-28 23:03:53 +0900

    companyのバリデーションを追加した

--- app/models/company.rb
+++ app/models/company.rb
@@ -3,4 +3,8 @@
 class Company < ApplicationRecord
   belongs_to :owner, class_name: 'User', foreign_key: :user_id, inverse_of: :companies
   has_many :works # rubocop:disable Rails/HasManyOrHasOneDependent
+  validates :name, presence: true, length: {in: 1..30}
+  validates :wage_amount, numericality: { in: 1..99999 }
+  validates :hourly_wage_system, presence: true
+  validates :currency_type, presence: true
 end
