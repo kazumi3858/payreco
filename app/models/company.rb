49929f69cd522b7117c3f6ca23b184f65c2bdb61
kazumi <kazumi121589@yahoo.co.jp>
2022-09-29 23:40:43 +0900

    バリデーションの修正をした

--- app/models/company.rb
+++ app/models/company.rb
@@ -5,6 +5,6 @@ class Company < ApplicationRecord
   has_many :works # rubocop:disable Rails/HasManyOrHasOneDependent
   validates :name, presence: true, length: { in: 1..30 }
   validates :wage_amount, numericality: { allow_nil: true, in: 1..99999 }
-  validates :hourly_wage_system, presence: true
+  validates :hourly_wage_system, inclusion: { in: [true, false] }
   validates :currency_type, presence: true
 end
