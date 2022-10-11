6e4f41573a2902d167a0d3953dd6ddf477e29465
kazumi <kazumi121589@yahoo.co.jp>
2022-10-11 11:27:34 +0900

    バリデーションを追加した

--- app/models/work.rb
+++ app/models/work.rb
@@ -4,7 +4,7 @@ class Work < ApplicationRecord
   belongs_to :user
   belongs_to :company, touch: true
   validates :date, presence: true
-  validates :pay_amount, presence: true
+  validates :pay_amount, presence: true, numericality: { in: 0..999999 }
   validates :working_hours, presence: true, numericality: { in: 0.01..47.99 }
   validates :memo, length: { allow_nil: true, in: 0..50 }
 end
